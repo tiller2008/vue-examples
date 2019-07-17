4016bca9a6c4d018b25cb0327c2529edb038fd26
tiller2008 <tiller@126.com>
2019-07-17 10:03:28 +0800

    完成7-1的学习

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    Hello World
+    <Header></Header>
   </div>
 </template>
 
 <script>
+import Header from './components/Header'
 // @ is an alias to /src
 export default {
-  name: 'home'
+  name: 'home',
+  components: { Header }
 }
 </script>
---
+++ src/views/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="header">
+    <div class="header-left">返回</div>
+    <div class="header-input">输入城市/景点/游玩主题</div>
+    <div class="header-right">城市</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    display: flex
+    line-height: 2.6875rem
+    background: #00BCD4
+    color: #FFF
+    .header-left
+      width: 2rem
+      float: left
+    .header-input
+      flex: 1
+      height: 1.9375rem
+      line-height: 1.9375rem
+      margin-top: .375rem
+      margin-left: .625rem
+      background: #FFF
+      border-radius: .15625rem
+      color: #CCC
+    .header-right
+      width: 3.875rem
+      float: right
+      text-align: center
+</style>
