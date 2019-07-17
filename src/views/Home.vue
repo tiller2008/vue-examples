3a3d7768bde3b5be7179b836ef5805de634360ac
tiller2008 <tiller@126.com>
2019-07-17 15:57:36 +0800

    7-3 swipper

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <Header></Header>
+    <HomeSwiper></HomeSwiper>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import HomeSwiper from './components/Swiper'
 // @ is an alias to /src
 export default {
   name: 'home',
-  components: { Header }
+  components: { HomeSwiper, Header }
 }
 </script>
---
+++ src/views/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="slide in swiperSlide" :key="slide.id">
+        <img class="swiper-img" :src="slide.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        loop: true
+      },
+      swiperSlide: [{
+        id: '0001',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/66f201b7c7702f9d2d51a1aed86b9f8f.jpg_750x200_179821e4.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/87a224d0349d94a11e97f31aa1aba4f5.jpg_750x200_1f78af87.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #FFF!important
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 26.67%
+    background: #EEE
+    .swiper-img
+      width: 100%
+</style>
