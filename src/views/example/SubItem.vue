509dc30e538f6313046a451aee81868868407651
tiller2008 <tiller@126.com>
2019-07-24 09:38:37 +0800

    test template+slot

---
+++ src/views/example/SubItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <slot name="a"></slot>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SubItem'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/example/Template.vue
+++ src/views/example/Template.vue
@@ -6,12 +6,24 @@
       </template>
     </ul>
     <p v-else>Nothing to show</p>
+    <br/>
+    测试具名插槽：
+    <SubItem>
+      <template v-slot:a>
+        <p>具名插槽，查找的是名为a的插槽</p>
+      </template>
+      <template>
+        <p>这里没有使用具名插槽，在SubItem中没有&lt;slot&gt;&lt;/slott&gt;时，这里不会显示</p>
+      </template>
+    </SubItem>
   </div>
 </template>
 <!-- 实验template中使用v-for -->
 <script>
+import SubItem from './SubItem'
 export default {
   name: 'Template',
+  components: { SubItem },
   data () {
     return {
       list: [{
